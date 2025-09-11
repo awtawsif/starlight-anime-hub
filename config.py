ee0ce1b761d1a832a6084f296301c52e1bcaf6cf
awtawsif <tawsif7492@gmail.com>
2025-09-11 17:05:38 +0600

    update cookie and site url | remove uv related files

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ This module contains configuration settings for the Anime API, HTTP headers, and
 """
 
 # Base URL for the animepahe API
-API_BASE_URL = "https://animepahe.ru/api"
+API_BASE_URL = "https://animepahe.si/api"
 
 # Base URL for anime detail pages (for scraping)
-ANIME_PAGE_BASE_URL = "https://animepahe.ru/anime"
+ANIME_PAGE_BASE_URL = "https://animepahe.si/anime"
 
 # Default HTTP headers to use for API requests and web scraping
 # These headers mimic a typical web browser request to avoid bot detection
@@ -16,8 +16,8 @@ API_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0',
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'X-Requested-With': 'XMLHttpRequest',
-    'Referer': 'https://animepahe.ru/',
-    'Cookie': '__ddg8_=YJOG0vO34DB3UbwT; __ddg10_=1752381610; __ddg9_=103.109.59.228; __ddgid_=2hXmxDnC41E10u3d; __ddgmark_=N92cqg6NvQTrMSTB; __ddg2_=FZF2QUR1OoUtn6W5; __ddg1_=jDF17M15htxHLqIAwo7P; res=1080; aud=jpn; av1=0; ddg_last_challenge=1752342417847; latest=6035; XSRF-TOKEN=eyJpdiI6IlBsd1M3emV1b3RoMTl4OTdvUTRhYUE9PSIsInZhbHVlIjoiUEtKa3NGb3hrOG1rNWx5QTlRR3NvMTI5YVkzMnRSaU1hbzZTSEwvQ29DUi9PVDJmaFhaMnh5c2hlT2xUbDFLWkIwVzRndFk2emhDeVBQeTVYWXBQaGtxVTBHT1YxNEszRkZhWXMvTFVWT1lnU296TUNJZitqUTM4d0QwY25ZOTciLCJtYWMiOiJmOWI2NTIyYzUyYTgyM2Q5MmZhZTUwMDRlNzJhZDFhOGQ2MmEwYTExYWJjNWIzODIxMmQ5NDEwNjRmYzM3ZWNmIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjdDcVB3eGNWYVZyTWpyY3IzYlpLaFE9PSIsInZhbHVlIjoiZjFscWJRQmJWV0ZReHdMd1UzNEkzR2tqSVlSSnJ3Rlk5OWptKzF3bnVCTVpKd2p3TGVHSzlzMGdMMDNmVnQ0WDRVeDRLcGdta2lUM2JlWUtWbVlqZGFnRFI3dXBpUW9MZjNJTG84ejN5RHNZcnVjMDN6NkJFZnVISExIbHBUSnoiLCJtYWMiOiJlM2E3ZDNmNDA1YWU2NmU1ODRkMjQwODNmMzE3MzI4ZGY4NWI4MDMyZjNmMGIyYmU5NTQ3YmI2YWZjNzllYzA3IiwidGFnIjoiIn0%3D',
+    'Referer': 'https://animepahe.si/',
+    'Cookie': '__ddgid_=iYWMAyRO4hf0okA6; __ddgmark_=RvCt0bqM4WqP1OmM; __ddg2_=IzuBL5o0aS1rhi2E; __ddg1_=ubd4cco9B88CLjKbptZd; ddg_last_challenge=1757580029138; latest=6266; __ddg8_=11tvp4DVa25P4RK3; __ddg10_=1757588181; __ddg9_=103.109.59.228; XSRF-TOKEN=eyJpdiI6InZyUitJQzhrVFN6U0JXSC9HK2JhcFE9PSIsInZhbHVlIjoiTDZnbzJ2NjVMWTY4TDhLaGxSdlcrTUxuSFc5RXVHcjJGTWU4UFlhd3hWLzdlVE1JVDJYTVdZdUZQb1BTTXpBRVlrTWsxbTJ0RXpmKzhRazUwUDZpNWFRVU04OXYrN3oyRTF2TURmTHQ1cTdObXJDeTJVM05mZXFuOThNbEM0ekYiLCJtYWMiOiIwM2NlOGI3Y2IxY2FkZTc2ZGExYjM4YzIwYjRlZDY0YjM4ZDYyZDA5MjdiYjQxYzU4ZDk2YTI3OWRhN2M3OGYyIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6InYyVjM3QmQ0UEx1THNxN0NUNDFwc2c9PSIsInZhbHVlIjoiMjJRWTVhS1d6SitkeG1aay94MTJXQy9KQXd5KzZBSUI5UWJTelpNczFWR29VY2VPZkt6eHZHcTdZWjJJSUxIL0hjazltSWlHWXE4VnFHV1RWTjJBekgva3h3empLbWdGUVhqcHl6eE9ISU9Rekc2UER5QVBDODhIaHhzWXFCVWwiLCJtYWMiOiJkYzY4YTg1NDdhMjQzNzRlMmViNTQ0MDkwMjRiODg4NmYzNWVjZTZmZWE1MTAxOTliMDU5YjUzZWRlZTEwYWRkIiwidGFnIjoiIn0%3D',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin'
